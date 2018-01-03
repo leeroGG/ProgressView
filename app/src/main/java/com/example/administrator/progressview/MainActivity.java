f1b1e7c8c9ba1fb71525ce3c60974be9d6d75541
leero <925230519@qq.com>
2018-01-03 20:31:13 +0800

    修改动态显示

--- app/src/main/java/com/example/administrator/progressview/MainActivity.java
+++ app/src/main/java/com/example/administrator/progressview/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.administrator.progressview;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,12 +17,17 @@ public class MainActivity extends AppCompatActivity {
         progressView2 = (ProgressView) findViewById(R.id.progress2);
         progressView3 = (ProgressView) findViewById(R.id.progress3);
 
-        // 动态效果
-        ProgressHelper helper = new ProgressHelper(progressView1, 65);
-        helper.start();
-
         // 默认模式
         progressView2.setProgress(50);
         progressView3.setProgress(100);
+
+        // 动态效果
+        findViewById(R.id.start).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ProgressHelper helper = new ProgressHelper(progressView1, 65);
+                helper.start();
+            }
+        });
     }
 }
